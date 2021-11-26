69f60188075f244e8ce74678bca935e1fef2e936
GuiHAO-GARY <guih9602@gmail.com>
2021-11-26 18:14:16 +0800

    Foundation

--- Python Foundation.py
+++ Python Foundation.py
@@ -1,3 +1,5 @@
+import math
+
 print('Hello, Algorithms and Data Structures')
 
 # ------------- Data ---------------------------
@@ -30,6 +32,7 @@ aString = 'a'
 # ----------- Built-in Set Data Type ----------------
 # List: A collection of elements which can be any data type, a sequential collection
 myList = [1, 3, True, 6.5]
+
 # Operation used to list
 myList[0]  # Index, remember the first position starts from index 0
 myList + ['new element']  # Concatenate
@@ -37,6 +40,7 @@ myList * 3  # Repeat myList 3 times and concatenate them together
 1 in myList  # Whether element 1 is in myList
 len(myList)  # Length of myList
 myList[1:2]  # Slicing: extract a part of myList. Left of : is selected while right of : is not selected
+
 # Method used to list
 myList.append('add_item')  # Add element at the ending of list
 myList.insert(0, 'insert_item')  # Insert at index 0
@@ -48,6 +52,11 @@ del myList[1]  # delete the item of mylist at index 1
 myList.count(True)  # Count frequency of True in myList
 myList.remove(6.5)  # Remove element 6.5 which appear firstly in myList
 
+# List Comprehension
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+tshirts = [(color, size) for color in colors for size in sizes]
+
 # range function generate a continuous sequence with start, end and step size
 range(10)
 range(0, 10)
@@ -56,7 +65,6 @@ list(range(1, 5))
 
 # String: Consists of alphabets, numbers and other symbols. A sequential collection
 myName = 'Gary'
-
 myName[2]
 myName * 2
 len(myName)
@@ -75,6 +83,31 @@ myName.split('a')  # ['G', 'ry']
 # Tuple is also immutable
 myTuple = (1, 2.9, True)
 # myTuple[0] = 1 will generate error
+# Tuple as records
+lax_coordinates = (33.45645, 453.45645)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+for country, _ in traveler_ids:
+    print(country)
+
+# Tuple unpacking
+latitude, longitude = lax_coordinates
+divmod(4, 8)
+divmod(*(4, 8))  # * unpack the tuple
+# Using * to grab excess items
+a, b, *rest = range(5)
+rest
+a, *body, c, d = range(5)
+body
+
+# Slicing Object
+a = list(range(5))
+a[slice(1, 2)]  # equal to a[1:2]
+a = ['string', 'apple', 'orange juice']
+sorted(a)
+sorted(a, key=len)
+a = ['1', 3, 34, '45', '2']
+sorted(a, key=int)
 
 # Set: No sequence. No repeat value
 mySet = {1, 1, 3, 4, 'cat', 5.5}
@@ -93,8 +126,30 @@ mySet.add('new element')
 mySet.pop()  # Randomly delete one element from set
 mySet.clear()  # Clear all elements in set
 
-
 # Dictionary: No sequence. A mapping from key to value.
+# Generate dictionary
+a = dict(one=1, two=2, three=3)
+b = {'one': 1, 'two': 2, 'three': 3}
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+d = dict([('one', 1), ('two', 2), ('three', 3)])
+e = dict(b)
+
+# dict comprehension
+DIAL_CODES = [
+    (86, 'China'),
+    (91, 'India'),
+    (1, 'United States'),
+    (62, 'Indonesia'),
+    (55, 'Brazil'),
+    (92, 'Pakistan'),
+    (880, 'Bangladesh'),
+    (234, 'Nigeria'),
+    (7, 'Russia'),
+    (81, 'Japan'),
+]
+country_code = {country: code for code, country in DIAL_CODES}
+{code: country.upper() for country, code in country_code.items() if code < 66}
+
 capitals = {'Iowa': 'DesMoines', 'Wisconsin': 'Madison'}
 
 capitals['Iowa']  # Index
@@ -107,3 +162,28 @@ capitals.items()  # dict_items([('Iowa', 'DesMoines'), ('Wisconsin', 'Madison')]
 capitals.get('Iowa')  # Similar with index
 capitals.get('China', 'No this capital')  # Index to a non-existing return with an alternative
 
+
+# Error Handling -------------------------------------------
+# 1. Try except
+def square_root(aNumber):
+    try:
+        print(math.sqrt(aNumber))
+    except:
+        print('Bad value for square root')
+        print('Use absolute value instead')
+        print(math.sqrt(abs(aNumber)))
+
+
+square_root(1)
+square_root(-1)
+
+
+# 2. raise to cast error
+def square_root(aNumber):
+    if aNumber >= 0:
+        print(math.sqrt(aNumber))
+    else:
+        raise RuntimeError('You cannot use a negative number!')
+
+
+square_root(-1)
